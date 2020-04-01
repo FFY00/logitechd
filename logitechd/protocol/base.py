6b5201069077f2ca9709a3317f56b50a8bb412cb
Filipe Laíns <lains@archlinux.org>
2020-04-01 23:34:50 +0100

    protocol: add BaseProtocol
    Signed-off-by: Filipe Laíns <lains@archlinux.org>

---
+++ logitechd/protocol/base.py
@@ -0,0 +1,16 @@
+# SPDX-License-Identifier: MIT
+
+import typing
+
+if typing.TYPE_CHECKING:  # pragma: no cover
+    import logitechd.device
+
+
+class BaseProtocol(object):
+    '''
+    Base protocol class
+    '''
+    def __init__(self, device: 'logitechd.device.Device', index: int, sw_id: int = 0):
+        self._device = device
+        self._index = index
+        self.sw_id = sw_id

c0dfdec3eae0bab327f703d8303534c014a610e7
Filipe Laíns <lains@archlinux.org>
2020-04-02 17:37:14 +0100

    protocol: hidpp20: refactor to use DocTable
    Signed-off-by: Filipe Laíns <lains@archlinux.org>

--- logitechd/protocol/hidpp20.py
+++ logitechd/protocol/hidpp20.py
@@ -1,51 +1,38 @@
 # SPDX-License-Identifier: MIT
 
-from typing import Any, Callable, ClassVar, Dict, List, Tuple, Union
+import collections
+import typing
+from typing import Any, Callable, ClassVar, Dict, List, NamedTuple, Tuple, Union
 
 import logitechd.hidpp
 import logitechd.protocol
 import logitechd.protocol.base
 import logitechd.utils
 
-
-class _DocEnum(object):
-    _str: ClassVar[Dict[int, str]] = {}
-
-    @classmethod
-    def get_str(cls, id: int) -> str:
-        return cls._str.get(id, 'Unkown')
-
-
-class _Features(_DocEnum):
+class _Features(metaclass=logitechd.utils.DocTable):
     '''
     Lists HID++ 2.0 Features
     '''
-    ROOT = 0x0001
-
-    _str = {
-        ROOT: 'IRoot',
-    }
+    ROOT = 0x0001, 'IRoot'
 
 
-class _Functions(_DocEnum):
+class _Functions(metaclass=logitechd.utils.DocTable):
     '''
     Lists HID++ 2.0 Functions
     '''
     # IRoot
-    GET_FEATURE = _Features.ROOT, 0
-    GET_PROTOCOL_VERSION = _Features.ROOT, 1
-
-    _str = {
-        GET_PROTOCOL_VERSION[1]: 'version, ping = GetProtocolVersion(ping)',
-    }
+    GET_FEATURE = 0, 'featureIndex, featureType = GetFeature(featureID)'
+    GET_PROTOCOL_VERSION = 1, 'version, ping = GetProtocolVersion(ping)'
 
 
-def hiddp_request(function: Tuple[int, int], sw_id: int = 0) -> Callable[[Callable[..., Any]], Callable[..., Any]]:
+def hiddp_request(feature: logitechd.utils.DocElement, function: logitechd.utils.DocElement, sw_id: int = 0) \
+        -> Callable[[Callable[..., Any]], Callable[..., Any]]:
     '''
     This decorator constructs a HID++ message with the requested HID++ 2.0
     function and passes it to the python function. It also injects a docstring
     to the function.
     '''
+    print(feature, function)
 
     def decorator(func: Callable[..., Any]) -> Callable[..., Any]:
         def wrapper(protocol: logitechd.protocol.base.BaseProtocol, *args: Any, **kwargs: Any) -> Any:
@@ -55,15 +42,15 @@ def hiddp_request(function: Tuple[int, int], sw_id: int = 0) -> Callable[[Callab
             kwargs['msg'] = HIDPP20.Message(
                 report_id=0x10,
                 device_index=protocol._index,
-                feature_index=function[0],
-                function=function[1],
+                feature_index=feature.value,
+                function=function.value,
                 sw_id=protocol.sw_id,
             )
 
             return func(protocol, *args, **kwargs)
         wrapper.__doc__ = f'HID++ 2.0 function wrapper\n\n' \
-                          f'Feature: {_Features.get_str(function[0])} (0x{function[0]:04x})\n' \
-                          f'Function: ({function[1]}) {_Functions.get_str(function[1])}'
+                          f'Feature: {feature.doc} (0x{feature.value:04x})\n' \
+                          f'Function: ({function.value}) {function.doc}'
         return wrapper
     return decorator
 
@@ -132,7 +119,7 @@ class HIDPP20(logitechd.protocol.base.BaseProtocol):
         '''
         return self.Message.from_buffer(msg.buffer)
 
-    @hiddp_request(functions.GET_PROTOCOL_VERSION)
+    @hiddp_request(features.ROOT, functions.GET_PROTOCOL_VERSION)  # type: ignore
     def ping(self, data: int, msg: 'HIDPP20.Message') -> int:
         msg.args[2] = data
 

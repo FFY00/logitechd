69b89eb83c64d83f7b2c347ffc4b6f738c2a02d4
Filipe Laíns <lains@riseup.net>
2021-04-10 20:19:20 +0100

    backend: add helper data
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- logitechd/backend.py
+++ logitechd/backend.py
@@ -1,8 +1,41 @@
 # SPDX-License-Identifier: MIT
 
 import abc
+import dataclasses
 
-from typing import Sequence, Set
+from typing import Optional, Sequence, Set
+
+
+# backend helper data
+
+
+@dataclasses.dataclass
+class _DeviceInfo(object):
+    bus: int = 0x03
+    vid: Optional[int] = None
+    pid: Optional[int] = None
+
+    def __str__(self) -> str:
+        if self.vid is not None and self.pid is not None:
+            return f'DeviceInfo({hex(self.bus)}, {hex(self.vid)}, {hex(self.pid)})'
+        return 'DeviceInfo(unknown)'
+
+
+_TARGET_DEVICES = [
+    # Wired
+    _DeviceInfo(vid=0x46d, pid=0xc33c),  # G513
+    _DeviceInfo(vid=0x46d, pid=0xc33e),  # G915
+    _DeviceInfo(vid=0x46d, pid=0xc33f),  # G815
+    # Receivers
+    _DeviceInfo(vid=0x46d, pid=0xc52b),  # Unifying
+    _DeviceInfo(vid=0x46d, pid=0xc539),  # Lightspeed 1.0
+    _DeviceInfo(vid=0x46d, pid=0xc53a),  # Powerplay (Lightspeed 1.0)
+    _DeviceInfo(vid=0x46d, pid=0xc53f),  # Lightspeed 1.1
+    _DeviceInfo(vid=0x46d, pid=0xc541),  # Lightspeed 1.1 v2
+]
+
+
+# backend abstractions
 
 
 class Device(metaclass=abc.ABCMeta):
